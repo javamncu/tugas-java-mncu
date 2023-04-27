d8731641142af67ae0b41e2991b4a56e5d781450
Adi <Adi@DESKTOP-AMT9S6T>
2023-04-27 14:24:05 +0700

    Initial Commit Tugas Pertemuan 2

---
+++ TugasPertemuan2/src/TugasPertemuan2/main.java
@@ -0,0 +1,7 @@
+package TugasPertemuan2;
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println("Gwejrwper");
+	}
+}
---
+++ TugasPertemuan2/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Adi
+ *
+ */
+module TugasPertemuan2 {
+}
